8e24c5e84040885d3f9b566601f2a01541d49417
Khanmohammedahad-netizen <Khanmohammedahad@yahoo.com>
2025-09-24 16:45:14 +0530

    Verify Netlify deployment configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MAK LLC</h1>
+        <p>Metal and Tire Export</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
